5d864495257f4ba7289866783ba485c38cd55ff5
brandjamie8 <brandjamie8@gmail.com>
2024-11-04 20:33:08 +0000

    Create app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+# app.py
+
+import streamlit as st
+
+st.set_page_config(page_title='Admitted Demand and Capacity', layout='wide')
+
+st.title('Welcome to the Admitted Demand and Capacity Analysis App')
+
+st.write("""
+This application allows you to analyze procedure demand, capacity, and waiting list dynamics.
+
+Use the navigation on the left to select different sections of the analysis.
+""")
