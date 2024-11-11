7c2d45f5a484c3630020f20ffc5273c33e4e5be6
brandjamie8 <brandjamie8@gmail.com>
2024-11-11 21:10:50 +0000

    Update 2_Demand.py

--- pages/2_Demand.py
+++ pages/2_Demand.py
@@ -63,8 +63,16 @@ if 'procedure_df' in st.session_state and st.session_state.procedure_df is not N
         st.write(f"**Baseline Year's Worth of Additions to Waiting List:** {baseline_yearly_additions:.0f}")
 
 
-
+        # Calculate demand minutes for the baseline period
+        baseline_procedure_df['demand minutes'] = baseline_procedure_df['total referrals'] * baseline_procedure_df['average duration']
+        total_demand_minutes_baseline = baseline_procedure_df['demand minutes'].sum()
         
+        # Print the calculated demand in minutes for the baseline period
+        st.write(f"**Total Demand in Minutes during Baseline Period:** {total_demand_minutes_baseline:.0f} minutes")
+
+        # Message about next steps
+        st.write("The next step is to analyze the time series of additions to the waiting list to determine whether there is an increase over time.")
+
         # Display total demand
         #total_demand_cases = procedure_specialty_df['total referrals'].sum()
         #total_demand_minutes = procedure_specialty_df['demand minutes'].sum()
