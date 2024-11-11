f350aca61dd6755af5ce6efeca770e464d88aad7
brandjamie8 <brandjamie8@gmail.com>
2024-11-11 13:27:02 +0000

    Update 2_Demand.py

--- pages/2_Demand.py
+++ pages/2_Demand.py
@@ -91,8 +91,8 @@ if ('waiting_list_df' in st.session_state and st.session_state.waiting_list_df i
         baseline_start_date = st.session_state.baseline_start_date
         baseline_end_date = st.session_state.baseline_end_date
         
-        baseline_start_date = pd.to_datetime(baseline_start_date).to_period('M').to_timestamp('M')
-        baseline_end_date = pd.to_datetime(baseline_end_date).to_period('M').to_timestamp('M')
+        baseline_start = pd.to_datetime(baseline_start_date).to_period('M').to_timestamp('M')
+        baseline_end = pd.to_datetime(baseline_end_date).to_period('M').to_timestamp('M')
         
         # Require a baseline period of at least 6 months
         if baseline_end <= baseline_start + pd.DateOffset(months=5):
